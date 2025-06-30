2a75079594bd6a9d922ad29091f0d3b4cc5d5212
Aurora School <info@auroraschool.hn>
2025-06-30 10:41:00 -0600

    Create products_data.js

---
+++ products_data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        id: 1,
+        url_image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 
+        name: 'Zapatos Nike',
+        price: '80'
+    },
+    {
+        id: 2,
+        url_image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 
+        name: 'Audifonos',
+        price: '20'
+    },
+    {
+        id: 3,
+        url_image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80', 
+        name: 'Reloj',
+        price: '50'
+    },
+    {
+        id: 4,
+        url_image: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80', 
+        name: 'Smartwatch',
+        price: '90'
+    },
+    {
+        id: 5,
+        url_image: 'https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80', 
+        name: 'Perfume',
+        price: '40'
+    },
+    
+]
